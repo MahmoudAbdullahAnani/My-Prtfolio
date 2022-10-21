1933df61de18372fde2e1a9cae60b05b0ea16104
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-10-21 13:59:18 +0200

    Push Project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React from "react";
+import LargNavbar from './layout/Navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <LargNavbar />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <h1 className='text-white'>Home</h1>
+          </>
+        } />
+        <Route path='/about' element={
+          <>
+            <h1 className='text-white'>about</h1>
+          </>
+        } />
+        <Route path='/projects' element={
+          <>
+            <h1 className='text-white'>projects</h1>
+          </>
+        } />
+        <Route path='/blogs' element={
+          <>
+            <h1 className='text-white'>blogs</h1>
+          </>
+        } />
+        <Route path='/contact' element={
+          <>
+            <h1 className='text-white'>contact</h1>
+          </>
+        } />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/Navbar/Navbar.js
@@ -0,0 +1,89 @@
+import React from "react";
+import './Navbar.css'
+
+
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import { Home } from "@mui/icons-material";
+import InfoIcon from '@mui/icons-material/Info';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import BookIcon from '@mui/icons-material/Book';
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+import { NavLink, useNavigate } from "react-router-dom";
+const LargNavbar = () => {
+    const [value, setValue] = React.useState('recents');
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const naveget = useNavigate()
+
+    return (
+        <>
+            {
+                window.innerWidth <= 996 ? <BottomNavigation className="fixed-bottom px-5 mx-2 navBtn mb-3 rounded" value={value} onChange={handleChange}>
+                    <BottomNavigationAction className="ps-5 active " onClick={() => naveget("/")}
+                        label="Home"
+                        value="home"
+                        icon={<Home />}
+                    />
+
+                    <BottomNavigationAction onClick={() => naveget("/about")}
+                        label="About"
+                        value="about"
+                        icon={<InfoIcon />}
+                    />
+                    <BottomNavigationAction onClick={() => naveget("/projects")}
+                        label="Projects"
+                        value="projects"
+                        icon={<AccountTreeIcon />}
+                    />
+                    <BottomNavigationAction onClick={() => naveget("/blogs")}
+                        label="Blogs"
+                        value="blogs"
+                        icon={<BookIcon />}
+                    />
+                    <BottomNavigationAction onClick={() => naveget("/contact")}
+                        className="pe-5"
+                        label="Contact"
+                        value="contact"
+                        icon={<PermContactCalendarIcon />} />
+                </BottomNavigation> :
+                    <nav className="navbar sticky-top navbar-dark bg-dark  navbar-expand-lg ">
+                        <div className="container">
+                            <div className="logo w-100 relative d-flex justify-content-between">
+                                <span className="navbar-brand logoText fw-bolder ">
+                                    <img src="" alt=""/>
+                                    <span className="text-danger">M</span>y <span className="text-danger">P</span>ortfolo</span>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                            </div>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link"  aria-current="page" to="/about">About</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link"  aria-current="page" to="/projects">Projects</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link"  aria-current="page" to="/blogs">Blogs</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link"  aria-current="page" to="/contact">Contact</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+            }
+
+
+        </>
+    );
+}
+
+export default LargNavbar;
