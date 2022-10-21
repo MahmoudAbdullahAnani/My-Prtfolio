3ecff52fa1ce522187edb84a921a4c22ec8b4d7d
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-10-21 14:12:12 +0200

    Push Project1

--- src/App.js
+++ src/App.js
@@ -7,31 +7,47 @@ function App() {
     <>
       <LargNavbar />
       <Routes>
-        <Route path='/' element={
-          <>
-            <h1 className='text-white'>Home</h1>
-          </>
-        } />
-        <Route path='/about' element={
-          <>
-            <h1 className='text-white'>about</h1>
-          </>
-        } />
-        <Route path='/projects' element={
-          <>
-            <h1 className='text-white'>projects</h1>
-          </>
-        } />
-        <Route path='/blogs' element={
-          <>
-            <h1 className='text-white'>blogs</h1>
-          </>
-        } />
-        <Route path='/contact' element={
-          <>
-            <h1 className='text-white'>contact</h1>
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <h1 className="text-white">Home</h1>
+              <h1 className="text-white">Rahma El3abeta</h1>
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <h1 className="text-white">about</h1>
+            </>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <>
+              <h1 className="text-white">projects</h1>
+            </>
+          }
+        />
+        <Route
+          path="/blogs"
+          element={
+            <>
+              <h1 className="text-white">blogs</h1>
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <h1 className="text-white">contact</h1>
+            </>
+          }
+        />
       </Routes>
     </>
   );
