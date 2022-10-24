c0bfea2cbb5aa3141aa17ccb14b5321c7f0b9fc1
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-10-24 21:45:29 +0200

    Edit CopyRight

--- src/layout/Footer/CopyRight.js
+++ src/layout/Footer/CopyRight.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./copyRight.css"
 const CopyRight = () => {
   return (
     //  position-absolute bottom-0 start-50 translate-middle-x w-100
-    <div className="text-center bg-dark py-2">
-      <p className="text-white m-0 py-2 fs-5 fw-bold">CopyRight Mahmoud Abdullah</p>
+    <div className="text-center CopyRight bg-dark pt-2">
+      <p className="text-white m-0 py-2 fs-5 fw-bold">
+        CopyRight Mahmoud Abdullah
+      </p>
     </div>
   );
 }
