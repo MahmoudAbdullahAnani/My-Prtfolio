8c541b3da5c9f67ff9831c3c36cc539232fcd24b
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-10-27 05:39:46 +0200

    Add countriy in api phone number

--- src/Components/Site.js
+++ src/Components/Site.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Site = (props) => {
   return (
-    <a href="https://api.whatsapp.com/send?phone=01028876202" className="">
+    <a href="https://api.whatsapp.com/send?phone=201028876202" className="">
       <i className={props.class}></i>
     </a>
   );
