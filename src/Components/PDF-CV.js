18217acb4ec6e1a5b35c328da7e3580c6a210a0d
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-12-14 07:12:23 +0200

    Edit link Download CV

--- src/Components/PDF-CV.js
+++ src/Components/PDF-CV.js
@@ -4,9 +4,11 @@ const PDFCV = () => {
     <>
       <div className="mt-4 ms-xs-auto ms-md-5">
         <a
-          href="./Mahmoud-Abdullah-Anani.pdf"
+          target="_blank"
+          href="https://doc-10-30-apps-viewer.googleusercontent.com/viewer/secure/pdf/h1ig076jtb5c4cmj26fcnooudmck7ihg/ceijphf4lansg60osr03m12k02rqidee/1670994675000/drive/07535918079148634039/ACFrOgCSESBGlmZZsRmOn2S_YrnlFCofaFJKDeEPqQGA67-C_elND6G2sRLTefbyaj6ES4__pMfUiUU2iPgSNQzlzDMKfv3ENpFSw6BULyGPs0usqfuTWxkkrR8fho_kagmBVGS-YszQYaPo__yz?print=true&nonce=hncitsd0v9510&user=07535918079148634039&hash=urki7afu8116pj47vv40rdurn3aqbkk1"
           className=""
           download="Mahmoud-Abdullah-Anani.pdf"
+          rel="noreferrer"
         >
           <button type="button" className="fs-5 fw-bold btn btn-primary">
             Download My CV
