746f33f3ab9a21a747ec3201ed3445f79718d30a
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2022-12-29 06:46:10 +0200

    Edit Link Download CV

--- src/Components/PDF-CV.js
+++ src/Components/PDF-CV.js
@@ -5,7 +5,7 @@ const PDFCV = () => {
       <div className="mt-4 ms-xs-auto ms-md-5">
         <a
           target="_blank"
-          href="https://doc-04-30-apps-viewer.googleusercontent.com/viewer/secure/pdf/h1ig076jtb5c4cmj26fcnooudmck7ihg/ea5t42fvg60g4pg9ap5pjtu4l49hldvs/1670994825000/drive/07535918079148634039/ACFrOgBR2fGV9oDZ0ZiMuA9znMhgxWdjd_VuFxFLJSYvmJrAv5B77L7pZ2YyYOexT6YyniR4A3Yhe693s-lWrzOSxfTFpUBfaBxxieUEwiUggd2JmLqBJzQCt8WuGev4szEqeqc1DNPUVvQx1gLW?print=true"
+          href="https://www.linkedin.com/dms/D4D06AQHT6Iz1zTy8yg/messaging-attachmentFile/0/1670620958977?m=AQIoISvDvx6WdwAAAYVcMbUe_VckcBHVxcHm3L7Y2jeYuay4Er-aa2ETTA&ne=1&v=beta&t=W5fxOWlvGMoZeCRGS8SZQHi3kg6lCmXYqCXlrtt-7Wg"
           className=""
           download="Mahmoud-Abdullah-Anani.pdf"
           rel="noreferrer"
