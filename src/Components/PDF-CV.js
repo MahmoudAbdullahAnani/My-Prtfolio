f12d6eac16a0da8a316174e945f6a512050492cf
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-08-24 19:33:00 +0300

    edit cv

--- src/Components/PDF-CV.js
+++ src/Components/PDF-CV.js
@@ -5,13 +5,13 @@ const PDFCV = () => {
       <div className="mt-4 ms-xs-auto ms-md-5">
         <a
           target="_blank"
-          href="https://www.linkedin.com/dms/D4D06AQHT6Iz1zTy8yg/messaging-attachmentFile/0/1670620958977?m=AQIoISvDvx6WdwAAAYVcMbUe_VckcBHVxcHm3L7Y2jeYuay4Er-aa2ETTA&ne=1&v=beta&t=W5fxOWlvGMoZeCRGS8SZQHi3kg6lCmXYqCXlrtt-7Wg"
+          href="https://fuzzy-lion-singlet.cyclic.cloud/cv/Mahmoud-Abdullah.pdf"
           className=""
           download="Mahmoud-Abdullah-Anani.pdf"
           rel="noreferrer"
         >
           <a
-            href="https://www.docdroid.net/file/download/6sJRKRB/mahmoud-abdullah-pdf.pdf"
+            href="https://fuzzy-lion-singlet.cyclic.cloud/cv/Mahmoud-Abdullah.pdf"
             download="Mahmoud-Abdullah-Anani.pdf"
             target={"_blank"}
             rel="noreferrer"
